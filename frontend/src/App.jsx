c4eb4027f4ddb0fc57f28eb0841ac9456ab67533
hashmatjibran <88573720+hashmatjibran@users.noreply.github.com>
2025-08-25 13:19:25 +0530

    css Error Fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React ,{useRef, useState} from "react";
+import React ,{ useState} from "react";
 import Navbar from "./components/navbar/Navbar";
 import Cart from "./pages/Cart/Cart";
 import Home from "./pages/Home/Home";
